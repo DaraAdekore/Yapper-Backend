7f949c8549b3ba64e55df028fadfd6d2349283f6
Dara Adekore <dara@Daras-MacBook-Pro.local>
2025-02-16 20:45:00 -0800

    Implemented search filters

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
     cors({
-        origin: `${process.env.FRONTEND_URL}:${3312}` || 'http://localhost:3000',
+        origin: `${process.env.FRONTEND_URL}` || 'http://localhost:3312',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
