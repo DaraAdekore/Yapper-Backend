ab1dae7a69d4e7806036591b5856cd7eed0797a1
Dara Adekore <dara@Daras-MacBook-Pro.local>
2025-02-17 10:17:56 -0800

    addes proxies

--- dist/index.js
+++ dist/index.js
@@ -25,7 +25,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: `${process.env.FRONTEND_URL}:${process.env.PORT}` || `http://localhost:${process.env.PORT}`,
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
     cors({
-        origin: `${process.env.FRONTEND_URL}:${process.env.PORT}` || `http://localhost:${process.env.PORT}`,
+        origin: process.env.FRONTEND_URL,
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
